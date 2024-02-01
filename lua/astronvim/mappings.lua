59060f36a03e4906dc685b206e4bb7da1537db02
AbhinavNmdo <abhaynam22@gmail.com>
2024-02-01 09:30:07 +0530

    Add vim-visual-multi plugin and fix the buffer switch shortcuts

--- lua/astronvim/mappings.lua
+++ lua/astronvim/mappings.lua
@@ -52,9 +52,9 @@ maps.n["<leader>pl"] = { "<cmd>AstroChangelog<cr>", desc = "AstroNvim Changelog"
 -- Manage Buffers
 maps.n["<leader>c"] = { function() require("astronvim.utils.buffer").close() end, desc = "Close buffer" }
 maps.n["<leader>C"] = { function() require("astronvim.utils.buffer").close(0, true) end, desc = "Force close buffer" }
-maps.n["<S-h>"] =
+maps.n["<S-l>"] =
   { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" }
-maps.n["<S-l>"] = {
+maps.n["<S-h>"] = {
   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
   desc = "Previous buffer",
 }
